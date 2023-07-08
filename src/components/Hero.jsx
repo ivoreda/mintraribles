a93338f2dc454cf56231b2997133e4a8a0550b83
ivoreda <ivoredafej@gmail.com>
2023-07-08 18:20:55 +0100

    added view nft button to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,12 @@
-import Identicon from 'react-identicons'
-import { setGlobalState, useGlobalState, truncate } from '../store'
+import Identicon from "react-identicons";
+import { setGlobalState, useGlobalState, truncate } from "../store";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
-  const [connectedAccount] = useGlobalState('connectedAccount')
+  const [connectedAccount] = useGlobalState("connectedAccount");
   const onCreatedNFT = () => {
-    setGlobalState('modal', 'scale-100')
-  }
+    setGlobalState("modal", "scale-100");
+  };
 
   return (
     <div
@@ -23,7 +24,10 @@ const Hero = () => {
           </p>
         </div>
 
-        <div className="flex flex-row mt-5">
+        <div
+          className="flex mt-5 md:flex-[0.5] text-white md:flex list-none flex-row justify-between
+        items-center flex-initial"
+        >
           <button
             className="shadow-xl shadow-black text-white
             bg-[#e32970] hover:bg-[#bd255f]
@@ -32,6 +36,15 @@ const Hero = () => {
           >
             Create NFT
           </button>
+
+          <Link
+            to="/market"
+            className="shadow-xl shadow-black text-white
+            bg-[#e32970] hover:bg-[#bd255f]
+            rounded-full cursor-pointer p-2"
+          >
+            View all NFTs
+          </Link>
         </div>
 
         <div className="w-3/4 flex justify-between items-center mt-5">
@@ -61,7 +74,7 @@ const Hero = () => {
         />
         <div className="flex justify-start items-center p-3">
           <Identicon
-            string={connectedAccount ? connectedAccount : 'Connect Your Wallet'}
+            string={connectedAccount ? connectedAccount : "Connect Your Wallet"}
             size={50}
             className="h-10 w-10 object-contain rounded-full mr-3"
           />
@@ -69,14 +82,14 @@ const Hero = () => {
             <p className="text-white font-semibold">
               {connectedAccount
                 ? truncate(connectedAccount, 4, 4, 11)
-                : 'Connect Your Wallet'}
+                : "Connect Your Wallet"}
             </p>
             <small className="text-pink-800 font-bold">@you</small>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
